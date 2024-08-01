603cb81dddf020aad15dba9f690bf214a52161d5
John Ramos <johnalex.ramosf@gmail.com>
2024-08-01 18:47:02 -0500

    Primer commit de proyectos, archivos y carpetas del bootcamp TalentoTech

---
+++ semana8/30Julio2024/archivos.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const datos = "este es el contenido de mi nuevo archivo";
+const ruta = "./resultado.txt";
+fs.writeFile(ruta,datos,(err)=>{
+ if(err) throw err;
+ else console.log("archivo creado correctamente");   
+}) 
---
+++ semana8/30Julio2024/calculadora.js
@@ -0,0 +1,12 @@
+let numero1 = 5,numero2=3;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let division = numero1 / numero2;
+let multiplicacion = numero1 * numero2;
+
+
+console.log('la suma de los números es: ',suma);
+console.log('la resta de los números es: ',resta);
+console.log('las multiplicación de los números es: ',multiplicacion);
+console.log('las division de los números es: ',division);
